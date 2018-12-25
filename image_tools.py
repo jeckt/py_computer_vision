8548d895a0bf4c55f37d79e28d88f07595289c16
Steven Nguyen <steven.nguyen89@gmail.com>
2018-12-25 14:32:48 -0500

    Add histogram equalisation function and bug fixes

--- image_tools.py
+++ image_tools.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
 
-import os
+# TODO(steve): Should add some rudimentary unit tests
 
-def get_image_list(path):
+import os
+from PIL import Image
+from numpy import uint8, array, histogram, interp
+
+def get_image_list(path: str) -> list:
     """Returns a list of filenames for all jpg images in a directory."""
 
     return list(os.path.join(path, f)
                 for f in os.listdir(path)
                 if f.endswith('.jpg'))
 
-def image_resize(image, size):
+def image_resize(image: Image, size: (int, int)) -> array:
     """Resize an image array using PIL."""
-    image = Image.fromarray(uint8(im))
+    image = Image.fromarray(uint8(image))
 
     return array(image.resize(size))
 
+def histogram_equalisation(image: Image, n_bins: int =256) -> (Image, array):
+    """Histogram equalisation of a grayscale image."""
+
+    # get image histogram
+    image_hist, bins = histogram(image.flatten(), n_bins, density=True)
+    cdf = image_hist.cumsum() # cumulative distribution function
+    cdf = 255 * cdf / cdf[-1] # normalise
+
+    # use linear interpolation of cdf to find new pixel values
+    new_image = interp(image.flatten(), bins[:-1], cdf)
+
+    return new_image.reshape(image.shape), cdf
+
 if __name__ == '__main__':
-    images_files = get_image_list('data')
-    for f in images_files:
-        print(f)
+    image = array(Image.open('data/AquaTermi_lowcontrast.jpg').convert('L'))
+    new_image, cdf = histogram_equalisation(image)
