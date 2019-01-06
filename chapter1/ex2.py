a15a1ee876a9a9d066cc3019ea85fc27207607b4
Steven Nguyen <steven.nguyen89@gmail.com>
2019-01-06 17:26:57 -0500

    refactor grayscale unsharp maskign into function

--- chapter1/ex2.py
+++ chapter1/ex2.py
@@ -7,17 +7,21 @@ import matplotlib.pyplot as plt
 
 image_file = '../data/sunset_tree.jpg'
 
+def unsharp_masking(image: array, amount: float) -> array:
+    """Sharpen image by subtracting the blurred version from original."""
+    if image.ndim < 3:
+        # grayscale image
+        blurred_image = filters.gaussian_filter(image, amount)
+    else:
+        blurred_image = filters.gaussian_filter(image, amount)
+
+    return (image + (image - blurred_image))
+
 if __name__ == '__main__':
     image = array(Image.open(image_file).convert('L'))
-    blurred_image = filters.gaussian_filter(image, 0.65)
-    sharp_image = image + (image - blurred_image)
+    sharp_image = unsharp_masking(image, 0.65)
 
-    Image.fromarray(image).show()
-    Image.fromarray(sharp_image).show()
-
-    """
     fig, axes = plt.subplots(1, 2)
     axes[0].imshow(image, cmap="gray")
     axes[1].imshow(sharp_image, cmap="gray")
     plt.show()
-    """
