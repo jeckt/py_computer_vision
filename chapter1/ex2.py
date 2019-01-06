0e0ab8dfe5c19b7c17efd7111479e7a18a1ecf49
Steven Nguyen <steven.nguyen89@gmail.com>
2019-01-06 17:41:38 -0500

    Add unsharp masking for color images

--- chapter1/ex2.py
+++ chapter1/ex2.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 
 from PIL import Image
-from numpy import array
+from numpy import array, zeros
 from scipy.ndimage import filters
 import matplotlib.pyplot as plt
 
-image_file = '../data/sunset_tree.jpg'
+image_file = '../data/sf_view1.jpg'
 
 def unsharp_masking(image: array, amount: float) -> array:
     """Sharpen image by subtracting the blurred version from original."""
     if image.ndim < 3:
         # grayscale image
-        blurred_image = filters.gaussian_filter(image, amount)
+        blur_image = filters.gaussian_filter(image, amount)
     else:
-        blurred_image = filters.gaussian_filter(image, amount)
+        blur_image = zeros(image.shape)
+        for i in range(3):
+            blur_image[:,:,i] = filters.gaussian_filter(image[:,:,i], amount)
+        blur_image = array(blur_image, 'uint8')
 
-    return (image + (image - blurred_image))
+    return (image + (image - blur_image))
 
 if __name__ == '__main__':
-    image = array(Image.open(image_file).convert('L'))
-    sharp_image = unsharp_masking(image, 0.65)
+    import scipy
+    image = array(Image.open(image_file))
+    sharp_image = unsharp_masking(image, 1.0)
 
     fig, axes = plt.subplots(1, 2)
-    axes[0].imshow(image, cmap="gray")
-    axes[1].imshow(sharp_image, cmap="gray")
+    axes[0].imshow(image)
+    axes[1].imshow(sharp_image)
     plt.show()
