68bb9eb295c5d95eb31649a68ea9dfa72053330c
Steven Nguyen <steven.nguyen89@gmail.com>
2019-01-06 16:53:40 -0500

    Complete chapter 1 exercise 1

---
+++ chapter1/ex1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# Gaussian blur exercise
+
+from PIL import Image
+from numpy import array
+from scipy.ndimage import filters
+import matplotlib.pyplot as plt
+
+sigmas = [5, 10, 15]
+image_file = '../data/empire.jpg'
+
+if __name__ == '__main__':
+    image = Image.open(image_file).convert('L')
+    images = list(filters.gaussian_filter(image, s) for s in sigmas)
+    images.insert(0, array(image))
+
+    fig, axes = plt.subplots(2, len(images))
+    for i, im in enumerate(images):
+        axes[0, i].imshow(im, cmap="gray")
+        axes[0, i].axis('off')
+
+        axes[1, i].contour(im)
+        axes[1, i].axis('off')
+
+    plt.show()
